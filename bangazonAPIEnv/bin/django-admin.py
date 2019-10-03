c6c2b0b36355f295932551c493f86b9b30f564b0
Karla Gallegos <kgalleg618@gmail.com>
2019-10-03 11:18:56 -0500

    added requirements.txt

---
+++ bangazonAPIEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kgallegos/workspace/python/bangazon-public-api-vendacien-caliphate/bangazonAPIEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
