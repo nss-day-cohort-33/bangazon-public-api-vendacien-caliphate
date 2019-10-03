c98da0b886034f9b172eb94ab00f517e9815c4d8
Steve Brownlee <chortlehoort@gmail.com>
2019-10-03 15:27:42 -0500

    Removed another environment directory

--- bangazonAPIEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/kgallegos/workspace/python/bangazon-public-api-vendacien-caliphate/bangazonAPIEnv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
