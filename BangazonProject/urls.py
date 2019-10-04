ae0f0748e57312fe01338329c141bb251d0d5e4e
Jake Scott <bemybanker@gmail.com>
2019-10-04 14:23:44 -0500

    merging

--- BangazonProject/urls.py
+++ BangazonProject/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
-<<<<<<< HEAD
-from bangazonAPI import models
-=======
 from bangazonAPI.models import *
->>>>>>> 63d09e35eb778c01657cc0dc7a07acf24e4dc78f
 
 router = routers.DefaultRouter(trailing_slash=False)
 
